f04aca38579042db3b3431fbd6ba6133bcbefe09
lhw-ren <yeshuasynovic@gmail.com>
2024-10-25 05:54:41 +0800

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "[ 🐐 | Raphael scholar😃 ]";
+const doNotDelete = "[ 🐐 | Math Major 🧑🏻‍🏫 ]";
 /**
 * @author NTKhang
 * @author: do not delete it
